8eaa732d304e0038c29095ad434bee9b1e0471fa
sejungkwak <71224770+sejungkwak@users.noreply.github.com>
2021-03-09 23:49:28 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// const marvel = document.querySelector('.marvel');
+// const dc = document.querySelector('.dc');
+
+// function stretch() {
+//     this.style.flex = 3;
+// } 
+
+// function normal() {
+//     this.style.flex = 1;
+// }
+
+// marvel.addEventListener('mouseenter', stretch);
+// marvel.addEventListener('mouseout', normal);
+// dc.addEventListener('mouseenter', stretch);
+// dc.addEventListener('mouseout', normal);
